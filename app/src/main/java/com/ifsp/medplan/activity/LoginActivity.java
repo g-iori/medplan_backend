58be1eb97965826b6cb7e2242cd3814dd635a2f3
vinicalmeida <vinicalmeida38@gmail.com>
2019-12-04 00:32:42 -0300

    versao 0.2

---
+++ app/src/main/java/com/ifsp/medplan/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.ifsp.medplan.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.ifsp.medplan.R;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText login_email, login_senha;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login_email = (EditText)findViewById(R.id.login_email);
+        login_senha = (EditText)findViewById(R.id.login_senha);
+    }
+    public void OnLogin(View view) {
+        String email = login_email.getText().toString();
+        String senha = login_senha.getText().toString();
+        String type = "login";
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(type, email, senha);
+    }
+
+    public void abrirCadastro(View view){
+        Intent i = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity(i);
+    }
+}
